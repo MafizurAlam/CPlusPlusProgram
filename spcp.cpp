5b04b7bd66404dc92809e4e698530b524bb76b41
Mafizur Alam <mafizuralam001@gmail.com>
2023-08-26 19:39:57 +0530

    profitloss

---
+++ spcp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float cp;
+    cout<<"Enter the Cost Price of the product: ";
+    cin>>cp;
+    float sp;
+    cout<<"Enter the Selling Price of the Product: ";
+    cin>>sp;
+    if(sp>cp){
+        cout<<"You are in Profit";
+    }
+    else if(sp==cp){
+        cout<<"No Profit and Loss";
+    }
+    else{
+        cout<<"You are in Loss";
+    }
+    return 0;
+}
