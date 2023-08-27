29ffc1dbff7d2fb41d07b68c966838200323b4d8
Mafizur Alam <mafizuralam001@gmail.com>
2023-08-27 17:15:20 +0530

    vowelconsonent

---
+++ .vscode/vowel_consonent.cpp
@@ -0,0 +1,27 @@
+//Write a Program to check whether  a given character is a vowel or a consonant
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    if(ch=='a'){
+        cout<<"This is the Vowel";
+    }
+    else if(ch=='e'){
+        cout<<"This is the Vowel";
+    }
+    else if(ch=='i'){
+        cout<<"This is the Vowel";
+    }
+    else if(ch=='o'){
+        cout<<"This is the Vowel";
+    }
+    else if(ch=='u'){
+        cout<<"This is the Vowel";
+    }
+    else{
+        cout<<"This is the Consonent";
+    }
+    return 0;
+}
