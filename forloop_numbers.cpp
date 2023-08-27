776e531e6a8006b45c94e2e596a2abf09586351d
Mafizur Alam <mafizuralam001@gmail.com>
2023-08-27 23:29:57 +0530

    Print Numbers from 1 to 100

---
+++ forloop_numbers.cpp
@@ -0,0 +1,8 @@
+//Print Numbers from 1 to 100
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1; i<=100; i++){
+        cout<<"The Numbers between from 1 to 100 is: "<<i<<endl;
+    }
+}
